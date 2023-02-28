ff145fe1746b9d1cc4f39b998a979ef88a47b1d0
daxiongshu <aixueer4ever@gmail.com>
2023-02-28 17:39:43 +0000

    fix

--- setup_amex.sh
+++ setup_amex.sh
@@ -1,5 +1,6 @@
 kaggle competitions download -c amex-default-prediction -f train_labels.csv -p /raid/data/ml/kaggle/amex
 kaggle datasets download -d raddar/amex-data-integer-dtypes-parquet-format -p /raid/data/ml/kaggle/amex
+mkdir -p /raid/data/ml/kaggle/amex
 cd /raid/data/ml/kaggle/amex
 unzip *.zip
 mkdir processed models
